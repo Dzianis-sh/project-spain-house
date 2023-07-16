e12d30d25f2a6deb8cc44d61e5aec93d2810075b
Dzianis <denshakh92@gmail.com>
2023-07-17 00:10:31 +0300

    add full search block to resale.html

--- js/main.js
+++ js/main.js
@@ -33,3 +33,33 @@ window.addEventListener("click", function (event) {
   const menu = document.querySelector(".mobile-menu");
   menu.classList.add("hidden");
 });
+
+// show/hide select
+
+let select = function () {
+  let selectHeader = document.querySelectorAll('.select__header');
+  let selectItem = document.querySelectorAll('.select__item');
+
+
+  selectHeader.forEach(item => {
+    item.addEventListener('click', selectToggle)
+  });
+
+  selectItem.forEach(item => {
+    item.addEventListener('click', selectChoose)
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest('.select'),
+      currentText = select.querySelector('.select__current');
+    currentText.innerText = text;
+    select.classList.remove('is-active');
+  }
+};
+
+select();
