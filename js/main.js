f03ec4cb3ab02ac7950521f0d43c4c90b129979b
Dzianis <denshakh92@gmail.com>
2023-07-17 18:21:34 +0300

    add responsive to search block

--- js/main.js
+++ js/main.js
@@ -40,7 +40,6 @@ let select = function () {
   let selectHeader = document.querySelectorAll('.select__header');
   let selectItem = document.querySelectorAll('.select__item');
 
-
   selectHeader.forEach(item => {
     item.addEventListener('click', selectToggle)
   });
