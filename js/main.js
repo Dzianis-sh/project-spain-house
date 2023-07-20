df01b5488949486598dcb2ccf9f2162a0a65e14f
Dzianis <denshakh92@gmail.com>
2023-07-20 21:32:44 +0300

    update pagination & responsive object.html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// header scroll hide
-
 // Hide header when scroll page
 
 let lastScroll = 0;
@@ -62,3 +60,72 @@ let select = function () {
 };
 
 select();
+
+
+function removeClass(selector, classname) {
+  document.querySelectorAll(selector).forEach(function (item) {
+    item.classList.remove(classname);
+  });
+}
+function addClass(selector, classname) {
+  document.querySelectorAll(selector).forEach(function (item) {
+    item.classList.add(classname);
+  });
+}
+
+const pageControls = document.querySelectorAll(".pagination__item");
+let activePage = 0;
+const lastPage = document.querySelectorAll(".buildings-pages > div").length - 1;
+
+function showPage(pageNum) {
+  removeClass(".pagination .pagination__item--active", "pagination__item--active");
+
+  addClass(`.pagination__item[data-page-num='${pageNum}']`, "pagination__item--active");
+
+  removeClass(".buildings-pages .buildings-pages--active", "buildings-pages--active");
+  activePage = pageNum;
+  addClass(`.buildings-pages > div:nth-child(${activePage + 1})`, "buildings-pages--active");
+
+  if (pageNum === 0) {
+    addClass(`.pagination__item[data-page="prev"]`, "event-none");
+  } else {
+    removeClass(`.pagination__item[data-page="prev"]`, "event-none");
+  }
+
+  if (pageNum === lastPage) {
+    addClass(`.pagination__item[data-page="next"]`, "event-none");
+  } else {
+    removeClass(`.pagination__item[data-page="next"]`, "event-none");
+  }
+}
+
+pageControls.forEach(function (pageBtn) {
+
+  pageBtn.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    if (pageBtn.dataset.page) {
+      if (pageBtn.dataset.page === "prev") {
+        if (activePage !== 0) {
+          showPage(activePage - 1);
+        }
+      } else if (pageBtn.dataset.page === "next") {
+        if (activePage < lastPage) {
+          showPage(activePage + 1);
+        }
+      }
+      return;
+    }
+
+    const targetPage = Number(pageBtn.getAttribute("data-page-num"));
+    if (targetPage != activePage) {
+      showPage(targetPage);
+
+      //buildings-pages--active
+    }
+  });
+
+})
+showPage(0);
+
