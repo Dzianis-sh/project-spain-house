7e280f8beb8d9f2a34a8d0ddebefedf1925b892d
Dzianis <denshakh92@gmail.com>
2023-08-10 15:30:32 +0300

    add first block slider in recomment objects->home page

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.slider', {
+  // Optional parameters
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
